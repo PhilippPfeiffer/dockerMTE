e005fff50ad80cbd551a3703bb583b6a15b579a9
Vader-San <vadersan@gmail.com>
2015-02-11 00:38:52 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,3 +7,5 @@ echo "=> Restarting Glassfish server"
 asadmin stop-domain
 echo "=> Starting and running Glassfish server"
 asadmin start-domain -w
+echo "=> Deploying webapp on Glassfish server"
+asadmin deploy MTE.war
