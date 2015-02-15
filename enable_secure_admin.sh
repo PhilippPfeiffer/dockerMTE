6138ec4a0147b26720f1145a2b4dc4a11063aa52
Vader-San <vadersan@gmail.com>
2015-02-15 15:52:36 +0100

    Update enable_secure_admin.sh

--- enable_secure_admin.sh
+++ enable_secure_admin.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/expect
 
+spawn asadmin --user admin change-admin-password
+expect "password"
+send "\n"
+expect "password"
+send "admin\n"
+expect "password"
+send "admin\n"
+expect eof
+
 spawn asadmin enable-secure-admin
 expect "admin"
 send "admin\n"
