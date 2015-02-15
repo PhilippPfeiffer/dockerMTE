ae95322f91e8da35053187c2976ee5cc15639800
Vader-San <vadersan@gmail.com>
2015-02-15 15:22:19 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 set -e
 
 asadmin start-domain
-echo "=> Changing admin password to admin"
-/change_admin_password.sh
 echo "=> Enabling secure admin login"
 /enable_secure_admin.sh
 echo "=> Restarting Glassfish server"
