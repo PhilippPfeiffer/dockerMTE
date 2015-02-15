d48f5cada308dad3b9a62c348a3f7b3415e40342
Vader-San <vadersan@gmail.com>
2015-02-15 16:34:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,12 +3,13 @@
 set -e
 
 asadmin start-domain
+echo "=> Deploying App"
+asadmin deploy MTE.war
 echo "=> Enabling secure admin login"
 /enable_secure_admin.sh
 echo "=> Restarting Glassfish server"
 asadmin stop-domain
 echo "=> Starting and running Glassfish server"
-asadmin start-domain 
-echo "=> Deploying App"
-asadmin deploy MTE.war -w
+asadmin start-domain -w
+
 
