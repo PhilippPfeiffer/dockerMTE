3a5bc1059a4c2764873e4c37b53d83387ae073b1
Vader-San <vadersan@gmail.com>
2015-02-15 14:40:47 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-#!/usr/bin/expect
 
 set -e
 
 asadmin start-domain
+echo "=> Enabling secure admin login"
+/enable_secure_admin.sh
 echo "=> Restarting Glassfish server"
 asadmin stop-domain
 echo "=> Starting and running Glassfish server"
-asadmin start-domain
-echo "=> Enabling secure admin login"
-set password admin
-spawn asadmin enable-secure-admin
-expect "admin"
-send "admin\n"
-expect "password"
-send "$password\n"
-expect eof
-exit
+asadmin start-domain -w
 
